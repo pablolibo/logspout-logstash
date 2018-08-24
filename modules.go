2bb55d5f7873492b24d0735faa28fa99d5eefafb
Pablo J. Villarruel <pablolibo@gmail.com>
2018-08-24 12:10:16 -0300

    Support multiline and healtcheck

--- modules.go
+++ modules.go
@@ -2,6 +2,8 @@ package main
 
 import (
   _ "github.com/looplab/logspout-logstash"
+  _ "github.com/gliderlabs/logspout/healthcheck"
+  _ "github.com/gliderlabs/logspout/adapters/multiline"
   _ "github.com/gliderlabs/logspout/transports/udp"
   _ "github.com/gliderlabs/logspout/transports/tcp"
 )
